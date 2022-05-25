563877940d9c1e4624a573cf37f0773ea8bc882a
ThiagoSosa0112 <99771836+ThiagoSosa0112@users.noreply.github.com>
2022-05-24 21:15:54 -0300

    Add files via upload

--- proyecto-final- js/javascript/miarchivo.js
+++ proyecto-final- js/javascript/miarchivo.js
@@ -1,84 +1,121 @@
-let stockProductos = [
-        {id: 1, nombre: "Aloe", precio: 800,img: '../media/producto12.jpeg'},
-        {id: 2, nombre: "Alegría", precio: 400,img: '../media/producto13.jpeg'},
-        {id: 3, nombre: "Maceta", precio: 1900, img: '../media/producto14.jpeg'},
-        {id: 4, nombre: "Ficus", precio: 400, img: '../media/producto16.jpeg'},
-        {id: 5, nombre: "Palmera", precio: 600, img: '../media/producto15.jpeg'},
-        {id: 6, nombre: "Cactus", precio: 200, img: '../media/producto17.jpeg'}
-    ] 
+const listaProductos = document.getElementById("ecommerce");
 
+class Producto {
+    constructor(title, description, price, img, id) {
+        this.title = title;
+        this.description = description;
+        this.price = price;
+        this.img = img;
+        this.id = id;
+    }
+}
+
+const aloe = new Producto("Aloe", "Planta de interior", 450, "../media/producto12.jpeg", 1);
+const palmera = new Producto("Palmera", "Planta de exterior", 450, "../media/producto12.jpeg", 2);
+const cactus = new Producto("Catus", "Planta de interior", 450, "../media/producto12.jpeg", 3);
+const alegria = new Producto("Alegria", "Planta de exterior", 900, "../media/producto12.jpeg", 4);
+const maceta = new Producto("Maceta", "Artículo" , 900, "../media/producto12.jpeg", 5);
+const tulipan = new Producto("Tulipan", "Planta de exterior", 900, "../media/producto12.jpeg", 6);
+ 
+const baseDeDatosRopa = [aloe, palmera, cactus, alegria, maceta, tulipan];
+ 
+const carrito = JSON.parse(localStorage.getItem('carrito')) || [];
 
  
-
-const dark = "click";
-let botonDark = document.getElementById("moon");
-botonDark.addEventListener ('click', () => {
-        let colorMode1 = document.getElementById("principal");
-        colorMode1.className = ("color");
-        let footer1 = document.getElementById("footer");
-        footer1.className = ("color");
-})
-let botonLight = document.getElementById("sun");
-botonLight.addEventListener ('click', () => {
-        let colorMode2 = document.getElementById("principal");
-        colorMode2.className = ("color2");
-        let footer2 = document.getElementById("footer");
-        footer2.className = ("color2");
-})
-
-
-const contenedorProductos = document.getElementById('contenedor-productos');
-
-const contenedorCarrito = document.getElementById('tablaCarrito')
-
-let carrito = [];
-
- stockProductos.forEach((producto) => {
-         const div = document.createElement('div');
-         div.classList.add('producto')
-         div.innerHTML = 
-         `
-         <img src= ${producto.img} alt= "">
-         <h3> ${producto.nombre}</h3>
-         <p class= "precioProducto">Precio: $ ${producto.precio} </p>
-         <button id="agregar${producto.id}" class= "boton-agregar"> Agregar al carrito <i class"fas fa-shopping-cart"></button>
-         `
-         contenedorProductos.appendChild(div);
-
-         const boton = document.getElementById(`agregar${producto.id}`)
-         boton.addEventListener ('click', () => {
-                let agregarCarrito = document.getElementById("tablaCarrito");
-        agregarCarrito.innerHTML +=  
-                `
-                <tr>
-                <th scope="row">${producto.id}</th>
-                <td>${producto.nombre}</td>
-                <td>$ ${producto.precio}</td>
-                </tr>
-                `
-
-         })
-        
-        })
-        
-        let ponerStorage = (clave, valor) =>{
-                localStorage.setItem(clave, valor)
-         }
-         for (const producto of stockProductos){
-                ponerStorage(producto.id, JSON.stringify(producto))
-         }
- 
- 
-//formulario 
-let formulario = document.getElementById("formulario");
-formulario.addEventListener ('submit', (e) => {
-    e.preventDefault();
-    let miForm = e.target;
-    console.log(formulario.children[0].value);
-    console.log(formulario.children[2].value);
-    console.log(formulario.children[4].value);
-    console.log(formulario.children[8].value);
+let acumulador = ``;
+baseDeDatosRopa.forEach((producto) => {
+    acumulador += `
+  <div class="col">
+    <div class="card" style="width: 20rem;">
+    <img src="${producto.img}" class="card-img-top imgProd" style="height: 16rem;" alt="...">
+      <div class="card-body text-center">
+        <h3 class="card-title titleProd" id="">${producto.title}</h3>
+        <h4 class="card-text descriptionProd" id="">${producto.description}</h4>
+        <h4 class="card-text priceProd" id="">$${producto.price}</h4>
+        <button data-id="${producto.id}" class="btn btn-primary agregar-carrito">Agregar</button>
+      </div>
+    </div>
+  </div>`;
 });
 
- 
- 
+
+listaProductos.innerHTML = acumulador;
+
+
+if (listaProductos) { listaProductos.addEventListener("click", agregarAlCarrito); }
+
+
+
+function agregarAlCarrito(e) {
+    e.preventDefault();
+
+    // Se localiza el click:
+    if (e.target.classList.contains("agregar-carrito")) {
+
+        const productoSeleccionado = e.target.parentNode;
+        console.log(productoSeleccionado);
+        obtenerDatos(productoSeleccionado);
+    }
+}
+function obtenerDatos(productoCard) {
+    const datosProducto = {
+        nombre: productoCard.querySelector(".titleProd").textContent,
+        modelo: productoCard.querySelector(".descriptionProd").textContent,
+        precio: productoCard.querySelector(".priceProd").textContent,
+        //Si tiene imagen sería accediento al atributo '.src':
+        img: productoCard.parentNode.querySelector(".imgProd").src
+    };
+
+    console.log(datosProducto);
+    carrito.push(datosProducto);
+    console.log(carrito);
+    guardarStorage();
+}
+
+function guardarStorage() {
+    localStorage.setItem('carrito', JSON.stringify(carrito));
+}
+
+const contenedorCarrito = document.querySelector("#carrito")
+const mostrarCarrito = document.querySelector("#mostrar-carrito")
+
+
+if (mostrarCarrito) {
+    mostrarCarrito.addEventListener('click', mostrarElCarrito)
+
+}
+function mostrarElCarrito() {
+    if (localStorage.length == 0) {
+        const msgInicial = document.createElement("h2")
+        msgInicial.innerHTML = "No hay productos en el carrito :("
+        contenedorCarrito.appendChild(msgInicial);
+    } else {
+        renderizarCarrito()
+    }
+}
+
+function renderizarCarrito() {
+    limpiarCarrito()
+    carrito.forEach(producto => {
+        const row = document.createElement('div');
+        row.classList.add("row")
+
+        row.innerHTML += `
+            <div class="col">
+                <img class="w-50" src="${producto.img}"/>
+            </div>
+            <div class="col">
+                <h2 class="">${producto.nombre}</h2>
+                <h4>${producto.precio}</h4>
+            </div>
+            <hr/>
+        `
+        contenedorCarrito.appendChild(row)
+    })
+}
+
+function limpiarCarrito() {
+    while (contenedorCarrito.firstChild) {
+        contenedorCarrito.removeChild(contenedorCarrito.firstChild)
+    }
+}
