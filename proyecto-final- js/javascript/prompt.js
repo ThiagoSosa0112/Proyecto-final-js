0716688a5bef4fa94cc7c773cf437d5c1a142313
ThiagoSosa0112 <99771836+ThiagoSosa0112@users.noreply.github.com>
2022-05-24 21:15:07 -0300

    Add files via upload

--- proyecto-final- js/javascript/prompt.js
+++ proyecto-final- js/javascript/prompt.js
@@ -1,7 +1,26 @@
-
+/*
 let mode = prompt("¿Quieres establecer el modo oscuro? Responda con si o con no.")
 let colorModeInicio = document.getElementById("principal");
 let colorModeFooter = document.getElementById("footer");
 mode === "si"?  colorModeInicio.className = ("color") : colorModeInicio.className = ("color2");
 mode === "si"?  colorModeFooter.className = ("color") : colorModeFooter.className = ("color2");
 mode==="si" && alert("¡Excelente! Modo oscuro activado, recuerda que puede desactivarlo haciendo click sobre el sol!");
+
+
+
+const dark = "click";
+let botonDark = document.getElementById("moon");
+botonDark.addEventListener ('click', () => {
+        let colorMode1 = document.getElementById("principal");
+        colorMode1.className = ("color");
+        let footer1 = document.getElementById("footer");
+        footer1.className = ("color");
+})
+let botonLight = document.getElementById("sun");
+botonLight.addEventListener ('click', () => {
+        let colorMode2 = document.getElementById("principal");
+        colorMode2.className = ("color2");
+        let footer2 = document.getElementById("footer");
+        footer2.className = ("color2");
+})
+*/
